3f7a34c23a061e8101a99930fadcac84f16b712f
Aishwarya <baishwarya77@gmail.com>
2022-06-29 15:02:50 +0530

    Add project files.

---
+++ MyfirstProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyfirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Aishu";
+            int number = 1;
+            Console.WriteLine("Name is \v" +name);
+                Console.ReadKey();
+        }
+    }
+}
